71835dab3b1c286b95620d673b4a0846703f1dbc
Felipe Tiecher <felipegtiecher@gmail.com>
2025-05-26 19:40:52 -0400

    atualizado para utilizar o controlador no registro de usuário

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,13 @@
 // backend/routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
+const authController = require('../controllers/authController'); // Importa o controlador
 
 // Rota de teste para authRoutes
 router.get('/test', (req, res) => {
     res.send('Auth routes estão funcionando!');
 });
+
+router.post('/register', authController.registerUser); // Rota para registro de usuário
 
 module.exports = router;
